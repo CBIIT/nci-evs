e10716b406dde7950ce595d649fc4917ad3c2eff
regulator95 <einolfs@nih.gov>
2022-09-24 19:16:41 -0400

    updating upgrade script. Needs to be rewritten

--- upgrade.sh
+++ upgrade.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-drush pmu -y ldap ldap_query ldap_help ldap_servers
-composer remove drupal/console drupal/module_missing_message_fixer drupal/backup_migrate drupal/core drupal-composer/drupal-scaffold --no-update
-composer require drupal/admin_toolbar:^2.5 drupal/ctools:^3.7 drupal/backup_migrate:^5.0.2 drupal/imce:^2.4 drupal/ldap_servers:^4.3 drupal/ldap:^4.3 drupal/ldap_query:^4.3 drupal/devel:^4.0  drush/drush:^10.0.0 drupal/core-recommended:^9.0.0 --no-update
-composer remove --dev webflo/drupal-core-require-dev --no-update
+drush sql-query "UPDATE users_field_data SET ldap_user_current_dn=NULL,ldap_user_last_checked=NULL,ldap_user_ldap_exclude=NULL;"
+drush pmu ldap_user ldap_help ldap_servers ldap_query ldap_authorization ldap_authentication ldap
+composer remove drupal/console
+composer remove drupal/core --no-update
+composer require drupal/core-recommended:^8.9.0 --no-update
 composer update
+composer remove drupal/core-composer-scaffold --no-update
+composer remove drupal/core-recommended --no-update
+composer update
+composer remove --dev webflo/drupal-core-require-dev --no-update
+composer remove drupal/core-composer-scaffold drupal/module_missing_message_fixer drupal/backup_migrate drupal/ldap --no-update
 
-drush updb -y
+composer require drupal/devel:^4.0 --no-update
+composer require drush/drush:^10.0.0 --no-update
+composer require drupal/core-recommended:9.0.0 --no-update
+composer update
