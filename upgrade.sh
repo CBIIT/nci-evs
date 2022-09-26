1b47332a433b15fb9cafa075797c519a3e43639d
regulator95 <einolfs@nih.gov>
2022-09-26 12:18:53 -0400

    was missing drupal/ in front of module_missing_message_fixer

--- upgrade.sh
+++ upgrade.sh
@@ -6,7 +6,7 @@ composer require drupal/admin_toolbar:^2.5 drupal/imce:^2.4 --no-update
 composer update
 composer remove --dev webflo/drupal-core-require-dev --no-update
 composer remove drupal/ldap drupal/devel drupal/console drupal/core webflo/drupal-finder drupal-composer/drupal-scaffold --no-update
-composer require module_missing_message_fixer drupal/authorization drush/drush:^10.0.0 drupal/backup_migrate drupal/core-composer-scaffold:^9 drupal/core-project-message:^9 --update-with-dependencies --no-update
+composer require drupal/module_missing_message_fixer drupal/authorization drush/drush:^10.0.0 drupal/backup_migrate drupal/core-composer-scaffold:^9 drupal/core-project-message:^9 --update-with-dependencies --no-update
 composer update
 drush cr
 drush pm:uninstall redirect_after_login
