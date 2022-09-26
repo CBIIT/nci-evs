c3625e637021ca853991b98c5d6ed401e7dfe13f
regulator95 <einolfs@nih.gov>
2022-09-26 09:50:26 -0400

    dropping and re-creating db on load

--- loaddb.sh
+++ loaddb.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+mysql -u$username -p$password -h$host -P$port -e "drop database evs;"
+mysql -u$username -p$password -h$host -P$port -e "create database evs;"
 drush sql-cli < database.sql
 drush ucrt a
 drush urol administrator a
