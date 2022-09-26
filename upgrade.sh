576ac7de8b2a76887a81cc803633a24e6cc8da7a
regulator95 <einolfs@nih.gov>
2022-09-26 11:48:46 -0400

    removing redirect after login module as it is breaking the login. Need to apply patch

--- upgrade.sh
+++ upgrade.sh
@@ -6,7 +6,7 @@ composer require drupal/admin_toolbar:^2.5 drupal/imce:^2.4 --no-update
 composer update
 composer remove --dev webflo/drupal-core-require-dev --no-update
 composer remove drupal/ldap drupal/devel drupal/console drupal/core webflo/drupal-finder drupal-composer/drupal-scaffold --no-update
-composer require drush/drush:^10.0.0 drupal/backup_migrate drupal/core-composer-scaffold:^9 drupal/core-project-message:^9 --update-with-dependencies --no-update
-rm composer.lock
+composer require drupal/authorization drupal/drush/drush:^10.0.0 drupal/backup_migrate drupal/core-composer-scaffold:^9 drupal/core-project-message:^9 --update-with-dependencies --no-update
 composer update
 drush cr
+drush pm:uninstall redirect_after_login
