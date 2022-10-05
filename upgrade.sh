5d7958970f0d1476b1a77d949d3b87244ff07e4d
Shaun Einolf <seinolf@gmail.com>
2022-10-05 11:26:44 -0400

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -8,7 +8,7 @@ drush updatedb --entity-updates -y
 drush updb -y
 composer remove --dev webflo/drupal-core-require-dev --no-update
 composer remove drupal/ldap drupal/devel drupal/console drupal/core webflo/drupal-finder drupal-composer/drupal-scaffold --no-update
-composer require drupal/ldap cweagans/composer-patches drupal/core-recommended:^9 drupal/module_missing_message_fixer drush/drush:^10.0.0 drupal/backup_migrate drupal/core-composer-scaffold:^9 drupal/core-project-message:^9 --update-with-dependencies --no-update
+composer require drupal/admin_toolbar:^3.2 drupal/ldap cweagans/composer-patches drupal/core-recommended:^9 drupal/module_missing_message_fixer drush/drush:^10.0.0 drupal/backup_migrate drupal/core-composer-scaffold:^9 drupal/core-project-message:^9 --update-with-dependencies --no-update
 composer update
 patch -u composer.json -i composer_redirect.patch
 composer install
