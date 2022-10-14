ee92781a415e6877f54ea7cf86a6eececae659d3
Your Name <you@example.com>
2022-10-14 14:00:22 -0400

    removing ldap_servers from pmu

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 drush sql-query "UPDATE users_field_data SET ldap_user_current_dn=NULL,ldap_user_last_checked=NULL,ldap_user_ldap_exclude=NULL;"
-drush pmu ldap_help ldap_servers ldap_user ldap_query ldap_authorization ldap_authentication ldap backup_migrate module_missing_message_fixer
+drush pmu ldap_help ldap_user ldap_query ldap_authorization ldap_authentication ldap backup_migrate module_missing_message_fixer
 composer remove drupal/backup_migrate drupal/ldap drupal/module_missing_message_fixer --no-update
 composer require drupal/admin_toolbar:^2.5 drupal/imce:^2.4 --no-update
 composer update
@@ -8,8 +8,7 @@ drush updatedb --entity-updates -y
 drush updb -y
 composer remove --dev webflo/drupal-core-require-dev --no-update
 composer remove drupal/ldap drupal/devel drupal/console drupal/core webflo/drupal-finder drupal-composer/drupal-scaffold --no-update
-composer require drupal/ldap cweagans/composer-patches drupal/core-recommended:^9 drupal/module_missing_message_fixer drush/drush:^10.0.0 drupal/backup_migrate drupal/core-composer-scaffold:^9 drupal/core-project-message:^9 --update-with-dependencies -
--no-update
+composer require drupal/ldap cweagans/composer-patches drupal/core-recommended:^9 drupal/module_missing_message_fixer drush/drush:^10.0.0 drupal/backup_migrate drupal/core-composer-scaffold:^9 drupal/core-project-message:^9 --update-with-dependencies --no-update
 composer update
 composer require drupal/easy_install 'drupal/devel_entity_updates:^3' drupal/admin_toolbar:^3 drupal/diff:^1.1 drupal/google_analytics
 composer update
